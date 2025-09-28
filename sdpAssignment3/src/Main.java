b099c6ae227b07263bdf1ded65cfa01212675440
kumawyy <241214@astanait.edu.kz>
2025-09-28 17:22:00 +0500

    Add files via upload

---
+++ sdpAssignment3/src/Main.java
@@ -0,0 +1,51 @@
+interface Bank {
+    void processPayment(double amount);
+}
+
+class KaspiBank {
+    public void kaspiPay(double sum) {
+        System.out.println("KaspiBank: payment of " + sum + " KZT completed successfully.");
+    }
+}
+
+class HalykBank {
+    public void halykTransfer(double money) {
+        System.out.println("HalykBank: transfer of " + money + " KZT executed.");
+    }
+}
+
+class KaspiBankAdapter implements Bank {
+    private KaspiBank kaspiBank;
+
+    public KaspiBankAdapter(KaspiBank kaspiBank) {
+        this.kaspiBank = kaspiBank;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        kaspiBank.kaspiPay(amount); // translate the call to Kaspi’s format
+    }
+}
+
+class HalykBankAdapter implements Bank {
+    private HalykBank halykBank;
+
+    public HalykBankAdapter(HalykBank halykBank) {
+        this.halykBank = halykBank;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        halykBank.halykTransfer(amount); // translate the call to Halyk’s format
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Bank kaspi = new KaspiBankAdapter(new KaspiBank());
+        kaspi.processPayment(15000);
+
+        Bank halyk = new HalykBankAdapter(new HalykBank());
+        halyk.processPayment(25000);
+    }
+}
